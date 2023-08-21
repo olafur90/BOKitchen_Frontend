5c3ccb48f8d3b4a417d00a2c68f54659be366d9c
Ólafur Pálsson <olp10@hi.is>
2023-08-21 10:41:29 +0000

    .

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	API_URL: 'mathollbackend-production.up.railway.app',
+	API_URL: 'https://mathollbackend-production.up.railway.app',
 };
 
 export const API_URL = environment.API_URL;
