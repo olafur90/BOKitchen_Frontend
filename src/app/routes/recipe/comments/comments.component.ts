11b80a3c775fede0e4dc9fc1b71ea1f7f0b180a8
Ólafur Pálsson <olp10@hi.is>
2023-08-24 21:58:14 +0000

    .

--- src/app/routes/recipe/comments/comments.component.ts
+++ src/app/routes/recipe/comments/comments.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Comment } from 'src/app/components/models/Comment';
 import { environment } from 'src/environments/environment';
@@ -25,23 +24,42 @@ import { environment } from 'src/environments/environment';
         ReactiveFormsModule
     ]
 })
+/**
+ * The comments component that displays and holds the logic for the comments for a recipe
+ */
 export class CommentsComponent implements OnInit {
+    
+    // The recipe id
     @Input()
     recipeId: number = 0;
 
-    
+    @ViewChild('formDirective') 
+    private formDirective: NgForm | undefined;
+
+    // An array to hold the fetched comments for the recipe
 	public comments: Comment[] = [];
 
+    public errors: boolean = false;
+
+    // The form for the comment input from the user
 	commentForm = new FormGroup({
-		name: new FormControl(''),
-		commentBody: new FormControl(''),
+		name: new FormControl('', Validators.minLength(3)),
+		commentBody: new FormControl('', Validators.minLength(3)),
 	})
 
+    /**
+     * The constructor
+     * @param http The HTTP client
+     * @param toastr The Toaster service
+     */
     constructor(
 		private http: HttpClient,
 		private toastr: ToastrService
 	) {}
 
+    /**
+     * The lifecycle hook that fetches the comments for the recipe and sorts them when the component is loaded
+     */
     async ngOnInit(): Promise<void> {
         this.http.get<Comment[]>(`${environment.API_URL}/uppskriftir/recipe/${this.recipeId}/comments`).subscribe((data) => {
             this.comments = data;
@@ -49,11 +67,18 @@ export class CommentsComponent implements OnInit {
         })
     }
 
+    /**
+     * Refreshes the comments component when it is called after updates have been made
+     * this.ngOnInit() could be called directly, but I find the code more readable with this method name
+     */
     refreshComments() {
         this.ngOnInit();
     }
 
-    onSubmit() {
+    /**
+     * A handler for the submit button for posting a new comment
+     */
+    onSubmit(): void {
 		if (this.commentForm.valid) {
 			const name = this.commentForm.get('name')?.value;
 			const commentBody = this.commentForm.get('commentBody')?.value;
@@ -65,13 +90,18 @@ export class CommentsComponent implements OnInit {
 			
 			this.http.post(`${environment.API_URL}/uppskriftir/recipe/${this.recipeId}/comments`, comment).subscribe();
 		
-			this.commentForm.reset();
+            this.commentForm.reset();
+            this.commentForm.markAsPristine();
+            this.commentForm.updateValueAndValidity();
 			
 			this.toastr.success('Athugasemd móttekin');
-
+            this.errors = false;
             setTimeout(() => {
                 this.refreshComments();
             }, 600);
 		}
+        else {
+            this.errors = true;
+        }
 	}
 }
