232ae8d5df39fba884d79b2f0bce2df53335febc
Ólafur Pálsson <olp10@hi.is>
2023-08-27 23:43:43 +0000

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-	title = 'matholl_client';
+export class AppComponent implements OnInit {
+	title = 'matholl_client'; 
+	initialized = false;
+
+	ngOnInit(): void {
+		// Check if user bobokitchen is logged in by checking localstorage
+		if (localStorage.getItem('username') === environment.username) {
+			this.initialized = true;
+		}
+	}
+
+	onLogin(userName: string, pass: string) {
+		if (userName === environment.username && pass === environment.password) {
+			this.initialized = true;
+		}
+		// Save something to sessionstorage
+		localStorage.setItem('username', userName);
+	}
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,7 @@ export const API_URL = 'https://mathollbackend-production.up.railway.app';
 
 export const environment = {
     production: true,
-    API_URL: API_URL
+    API_URL: API_URL,
+    username: 'bobokitchen',
+    password: 'mathollbola'
 }
